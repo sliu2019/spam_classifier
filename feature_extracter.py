7fed5146646ba1cfafb003dfae8328b6be321412
Chris Mitchell <13984870+cmitch@users.noreply.github.com>
2018-04-23 20:47:26 -0700

    Wrote test function, updated tokenizers

--- feature_extracter.py
+++ feature_extracter.py
@@ -1,11 +1,12 @@
 
 from bs4 import BeautifulSoup as BS
 from nltk.stem import WordNetLemmatizer
+from nltk import tokenize
 import string
 
 def word_counts(html):
 	lem = WordNetLemmatizer()
-	tokens = word_tokenize(BS(html).get_text())
+	tokens = tokenize(BS(html).get_text())
 	tokens = [lem.lemmatizer(token.lowercase) in tokens if token not in string.punctuation]
 
 	return make_dict(tokens)
@@ -27,21 +28,21 @@ def img_count(html):
 	return len(soup.find_all('img'))
 
 def junk_count(html):
-	tokens = word_tokenize(BS(html).get_text())
+	tokens = tokenize(BS(html).get_text())
 	tokens = [token in tokens if not token.isalpha() and token not in string.punctuation]
 	return len(tokens)
 
 
 def word_count(html):
-	tokens = word_tokenize(BS(html).get_text())
+	tokens = tokenize(BS(html).get_text())
 	return len([token in tokens if token not in string.punctuation])
 
 def doc_length(html):
-	tokens = word_tokenize(BS(html).get_text())
+	tokens = tokenize(BS(html).get_text())
 	return sum([len(token) in tokens if token not in string.punctuation])
 
 def longest_word(html):
-	tokens = word_tokenize(BS(html).get_text())
+	tokens = tokenize(BS(html).get_text())
 	return max([len(token) in tokens])
 
 def ave_word_length(html):
@@ -51,7 +52,7 @@ def ave_word_length(html):
 	return 0
 
 def punc_count(html):
-	tokens = word_tokenize(BS(html.get_text()))
+	tokens = tokenize(BS(html.get_text()))
 	count = 0
 	for token in tokens:
 		if len(token) > 1:
@@ -61,4 +62,16 @@ def punc_count(html):
 					break
 
 	return count
+
+def test(html):
+	print("Word counts:", word_counts(html))
+	print("Link count:", link_count(html))
+	print("Img count:", img_count(html))
+	print("Junk count:", junk_count(html))
+	print("Total word count:", word_count(html))
+	print("Doc length", doc_length(html))
+	print("Longest Word:", longest_word(html))
+	print("Ave word length:", ave_word_length(html))
+	print("Punc count:", punc_count(html))
+		
 
