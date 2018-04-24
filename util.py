752d85e0bee9700aee0811ce0b5a374b90c72c8e
wuhaosen <haosenwu@usc.edu>
2018-04-23 21:07:57 -0700

    merging api

---
+++ util.py
@@ -0,0 +1,60 @@
+import numpy as np 
+def main():
+    ##Simin
+    word_sophistication = False
+    cap_word_count = False
+    extra_cap = False
+    list_count = False
+
+    ##Chris
+    misspelled_word = False
+    longest_conseq_cap = False
+    HTML_check = False
+    style_check = False
+
+    ##Wiliam
+    image_count = False
+    link_count = False
+    number_count = False
+
+    ##Patrick
+    count_non_English = False
+    word_count = False
+    longest_conseq_char = False
+    character_count = False
+
+    output = []
+    if word_sophistication:
+        pass
+    if cap_word_count:
+        pass
+    if extra_cap:
+        pass
+    if list_count:
+        pass
+    if misspelled_word:
+        pass
+    if longest_conseq_cap:
+        pass
+    if HTML_check:
+        pass
+    is style_check:
+        pass
+    if image_count:
+        pass
+    if link_count:
+        pass
+    if number_count:
+        pass
+    if count_non_English:
+        pass
+    if word_count:
+        pass
+    if longest_conseq_char:
+        pass
+    if character_count:
+        pass
+    output_array = np.arrays(output)
+
+if __name__ = '__main__':
+    main()
