bd1a04a734c073b9b7e39669aafff46ba6260229
SIMIN LIU <simin.liu@berkeley.edu>
2018-05-02 23:50:22 -0700

    made classifiers object classes

--- lda.py
+++ lda.py
@@ -1,4 +1,4 @@
-class QDA(object):
+class LDA(object):
 
 	def __init__(self):
     pass
@@ -11,9 +11,9 @@ class QDA(object):
 
 	class_ids, counts = np.unique(Y_train, return_counts = True)
 	self.class_means = {}
-	self.class_covs = {}
 	self.class_priors = {}
 
+	self.cov = np.zeros((d, d))
 	for i in range(class_ids.size):
 		class_id = class_ids[i]
 
@@ -22,8 +22,8 @@ class QDA(object):
 		class_cov = (1.0/class_data.shape[0])*(X_train - class_mean).T @ (X_train - class_mean)
 
 		self.class_means[class_id] = class_mean
-		self.class_covs[class_id] = class_cov
 		self.class_priors[class_id] = counts[i]/np.sum(counts)
+		self.cov = self.cov + class_cov*class_priors[class_id]
 	return None
 
 	def predict(self, X_test):
@@ -37,12 +37,11 @@ class QDA(object):
 		best_class_id = None
 		best_class_value = float('-inf')
 		for class_id in class_ids: 
-			sigma = self.class_covs[class_id]
 			mu = self.class_means[class_id]
 			prior = self.class_priors[class_id]
 
 			x_i = X_test[i, :]
-			value = -1.0/2*log(np.linalg.det(sigma)) - 1.0/2*(x_i - mu).T @ np.linalg.inv(sigma) @ (x_i - mu) + log(prior)
+			value = -1.0/2*log(np.linalg.det(self.cov)) - 1.0/2*(x_i - mu).T @ np.linalg.inv(self.cov) @ (x_i - mu) + log(prior)
 
 			if value > best_class_value:
 				best_class_value = value
